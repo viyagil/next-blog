ad36171ac3d49fb7ab1ffec8d6cdb1cccc488467
bylim <bylim@woowahan.com>
2023-07-06 14:53:36 +0900

    레이아웃 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,17 +13,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className="flex flex-col h-screen">
-        <header className="flex justify-between">
-          <h1>Blog</h1>
-          <nav className="grid gap-4 grid-cols-4">
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/posts">Posts</Link>
-            <Link href="/contact">Contact</Link>
-          </nav>
-        </header>
-        {children}
-        <footer className="mt-auto">All Right Reserved.</footer>
+        <div className="container mx-auto">
+          <header className="flex justify-between mb-10">
+            <h1>Blog</h1>
+            <nav className="grid gap-4 grid-cols-4">
+              <Link href="/">Home</Link>
+              <Link href="/about">About</Link>
+              <Link href="/posts">Posts</Link>
+              <Link href="/contact">Contact</Link>
+            </nav>
+          </header>
+          {children}
+        </div>
+        <footer className="mt-auto text-center pt-4">All Right Reserved.</footer>
       </body>
     </html>
   )
